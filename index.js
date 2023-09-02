87ac6239c4c078859828114156ee0d09a59d2f3d
Ayush <ayush.up105@gmail.com>
2023-09-02 14:29:11 +0530

    modifified

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ require('./models/imageDetails')
 const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
+const PORT = process.env.PORT || 5000
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/image', require('./routes/images'));
 
-app.listen(5000,()=>{
+app.listen(PORT,()=>{
     console.log("Connected to http://localhost:5000");
 })
